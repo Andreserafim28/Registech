bbf362967ee99c90ed55a5df0a2e5a02d51f7912
Andreserafim28 <74571147+Andreserafim28@users.noreply.github.com>
2026-05-16 12:07:26 +0100

    Update ligacao.php

--- base_de_dados/ligacao.php
+++ base_de_dados/ligacao.php
@@ -1,12 +1,10 @@
 <?php
-// Configurações para o Railway
 $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQLDATABASE');
 $port = getenv('MYSQLPORT');
 
-// Estabelece a conexão usando a porta correta
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
