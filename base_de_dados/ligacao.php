0d4ed0ee53c079ef0788778ecd295a78fb187eba
Andreserafim28 <74571147+Andreserafim28@users.noreply.github.com>
2026-05-16 12:39:49 +0100

    Update ligacao.php

--- base_de_dados/ligacao.php
+++ base_de_dados/ligacao.php
@@ -1,23 +1,13 @@
 <?php
-
-
 $host = getenv('MYSQLHOST');
 $user = getenv('MYSQLUSER');
 $pass = getenv('MYSQLPASSWORD');
 $db   = getenv('MYSQLDATABASE');
 $port = getenv('MYSQLPORT');
 
-echo "Tentando ligar a: $host na porta $port...<br>";
-
-if (!function_exists('mysqli_connect')) {
-    die("ERRO CRÍTICO: A extensão MySQLi ainda não está ativa no servidor!");
-}
-
 $conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    die("Falha na ligação: " . mysqli_connect_error());
-} else {
-    echo "Ligação estabelecida com sucesso!";
+    die("Erro na ligação.");
 }
 ?>
